4d44792c2405eaa1f06ded15c6a5af583d84e142
Chris <chris@dutronlabs.com>
2015-05-18 14:38:37 -0400

    Revert "The Provision method currently doesn't return any connection data"
    This reverts commit e8c5d4712d4b0109a774f378fe48d3f81e715582.

--- Apprenda-Azure-Addon/Azure Storage/AzureStorageFactory.cs
+++ Apprenda-Azure-Addon/Azure Storage/AzureStorageFactory.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Apprenda.SaaSGrid.Addons.Azure.Storage
 {
@@ -9,114 +7,12 @@ namespace Apprenda.SaaSGrid.Addons.Azure.Storage
         // AI-121
         internal static OperationResult CreateBlobContainer(DeveloperParameters developerParameters)
         {
-            OperationResult result = new OperationResult();
-            CloudStorageAccount account;
-            result.EndUserMessage += "Parsing Connection String, attempting to connect to Storage\n";
-
-            try
-            {
-                account =  //not sure how correct this connection string is, especially around the "AccountKey" part
-                    CloudStorageAccount.Parse("DefaultEndpointsProtocol=[http];" + "AccountName={0};" + developerParameters.StorageAccountName + "AccountKey={1}" + developerParameters.AzureAuthenticationKey);
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage += "\nConnection String Parsed\n";
-
-            
-            CloudBlobClient client;
-            result.EndUserMessage += "\nCreating Blob Client\n";
-            try
-            {
-                client = account.CreateCloudBlobClient();
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage = "\nCreated Blob Client\n";
-
-
-            CloudBlobContainer container;
-            result.EndUserMessage = "\nCreating Blob Container\n";
-            try
-            {
-                container = client.GetContainerReference("mhow"/*gonna need the container name here*/);
-                container.CreateIfNotExists();
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage += "\nCreated Blob Container\n";
-
-
-            result.EndUserMessage += "\nSuccess!\n";
-            result.IsSuccess = true;
-            return result;
+            throw new NotImplementedException();
         }
 
         internal static OperationResult DeleteBlobContainer(DeveloperParameters developerParameters)
         {
-            OperationResult result = new OperationResult();
-            CloudStorageAccount account;
-            result.EndUserMessage += "Parsing Connection String, attempting to connect to Storage\n";
-
-            try
-            {
-                account =  //not sure how correct this connection string is, especially around the "AccountKey" part
-                    CloudStorageAccount.Parse("DefaultEndpointsProtocol=[http];" + "AccountName={0};" + developerParameters.StorageAccountName + "AccountKey={1}" + developerParameters.AzureAuthenticationKey);
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage += "\nConnection String Parsed\n";
-
-
-            CloudBlobClient client;
-            result.EndUserMessage += "\nCreating Blob Client\n";
-            try
-            {
-                client = account.CreateCloudBlobClient();
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage = "\nCreated Blob Client\n";
-
-
-            CloudBlobContainer container;
-            result.EndUserMessage = "\nDeleting Blob Container\n";
-            try
-            {
-                container = client.GetContainerReference("mhowa"/*gonna need the container name here*/);
-                container.DeleteIfExists();
-            }
-            catch (Exception e)
-            {
-                result.EndUserMessage += ("Exception:\n" + e.Message);
-                result.IsSuccess = false;
-                return result;
-            }
-            result.EndUserMessage += "\nDeleted Blob Container\n";
-
-
-            result.EndUserMessage += "\nSuccess!\n";
-            result.IsSuccess = true;
-            return result;
+            throw new NotImplementedException();
         }
 
         internal static OperationResult CreateQueue(DeveloperParameters developerParameters)
