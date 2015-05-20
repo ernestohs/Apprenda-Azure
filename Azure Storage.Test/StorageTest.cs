061a305505773ce79a69131182c97295bfe0c9d9
Chris <chris@dutronlabs.com>
2015-05-20 15:07:24 -0400

    Added the newly formed SaaSGrid.API NuGet package for 6.0, also stubbed out Azure Storage Tests

---
+++ Azure Storage.Test/StorageTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Azure_Storage.Test
+{
+    [TestFixture]
+    public class StorageTest
+    {
+        [SetUp]
+        public void SetupManifest()
+        {
+            // create the manifest and the developer options       
+        }
+
+        [Test]
+        public void ProvisionTest()
+        {
+        }
+
+        [Test]
+        public void DeProvisionTest()
+        {
+            
+        }
+
+        [Test]
+        public void SocTest()
+        {
+            
+        }
+    }
+}
